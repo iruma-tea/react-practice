dd90a1a35d00fc010bd5d40795d3eabe45d0020b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-06-17 19:22:39 +0900

    リスト1-3-14まで学習完了

---
+++ my-modern/obj_computed.js
@@ -0,0 +1,10 @@
+let i = 0;
+
+const member = {
+    [`attr${++i}`]: '佐藤理央',
+    [`attr${++i}`]: '女性',
+    [`attr${++i}`]: '18歳',
+    
+};
+
+console.log(member);
---
+++ my-modern/template.js
@@ -0,0 +1,3 @@
+const fullname = 'Taro Suzuki';
+const msg = `Hello, ${fullname} How are you today?`;
+console.log(msg);
