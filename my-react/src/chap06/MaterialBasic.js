6ac698a50f168ebe95627115564f1f99ea204db8
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-04 17:50:02 +0900

    6-1-3まで学習完了

--- my-react/src/chap06/MaterialBasic.js
+++ my-react/src/chap06/MaterialBasic.js
@@ -1,11 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Button } from "@mui/material";
 
 export default function MaterialBasic() {
+  // テキストの大文字/小文字変換を無効化
+  const font = css`
+    text-transform: none;
+  `;
   return (
     <>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="text" css={font}>
+        Text
+      </Button>
+      <Button variant="contained" css={font}>
+        Contained
+      </Button>
+      <Button variant="outlined" css={font}>
+        Outlined
+      </Button>
     </>
   );
 }
