d658e9d998a0bb78fd524c483bdc4d95ca070527
iruma-tea <shusuke.think.pad@gmail.com>
2024-08-13 17:49:48 +0900

    reviewの登録まで動作確認済み

--- my-next/src/app/edit/[id]/page.js
+++ my-next/src/app/edit/[id]/page.js
@@ -1,3 +1,17 @@
-export default function EditPage({ params }) {
-  return <p>No.{params.id}のレビューを表示しています。</p>;
+import BookDetails from "@/components/BookDetails";
+import FormEdit from "@/components/FormEdit";
+import { getBookById, getReviewById } from "@/lib/getter";
+
+export default async function EditPage({ params }) {
+  const book = await getBookById(params.id);
+  const review = await getReviewById(params.id);
+  const read = (review?.read || new Date()).toLocaleDateString("sv-SE");
+
+  return (
+    <div id="form">
+      <BookDetails book={book} />
+      <hr />
+      <FormEdit src={{ id: book.id, read, memo: review?.memo }} />
+    </div>
+  );
 }
